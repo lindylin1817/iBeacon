95e03a0187f35dd73018421ff81015b91d440151
shao ling <13911256582@163.com>
2014-05-27 17:07:09 +0800

    Initial Commit

---
+++ iBeacon/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  iBeacon
+//
+//  Created by ShaoLing on 27/5/14.
+//  Copyright (c) 2014 dastone. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ iBeacon/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  iBeacon
+//
+//  Created by ShaoLing on 27/5/14.
+//  Copyright (c) 2014 dastone. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
