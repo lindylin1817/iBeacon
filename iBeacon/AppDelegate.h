2d3d2512607c377ce21fbca64549655dce12e0a3
shao ling <13911256582@163.com>
2014-06-30 23:03:24 +0800

    Revert 1641fcb..22c6877
    This rolls back to commit 1641fcb3493266d11ccb1edf11742732ac1123cd.

--- iBeacon/AppDelegate.h
+++ iBeacon/AppDelegate.h
@@ -7,9 +7,13 @@
 //
 
 #import <UIKit/UIKit.h>
+#import "beaconCalibrate.h"
+
+@class beaconCalibrate;
 
 @interface AppDelegate : UIResponder <UIApplicationDelegate>
 
 @property (strong, nonatomic) UIWindow *window;
+@property (strong, nonatomic) IBOutlet beaconCalibrate *mybeaconCalibrate;
 
 @end
---
+++ iBeacon/beconCalibration.h
@@ -0,0 +1,13 @@
+//
+//  beconCalibration.h
+//  iBeacon
+//
+//  Created by Yonghua Lin on 28/6/14.
+//  Copyright (c) 2014 dastone. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface beconCalibration : NSObject
+
+@end
